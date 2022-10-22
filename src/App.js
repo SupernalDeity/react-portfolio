a9ea0c6bf2385c65cbd69315c534711527b2f26b
Anthony <perezant121@gmail.com>
2022-10-21 20:50:04 -0400

    contact layout and nav lights up to display current page

--- src/App.js
+++ src/App.js
@@ -8,45 +8,66 @@ import Contact from './pages/Contact'
 import Resume from './pages/Resume'
 import './assets/styles.css';
 
+let aboutOpacity = 'text-opacity-50';
+let portfolioOpacity = 'text-opacity-50';
+let contactOpacity = 'text-opacity-50';
+let resumeOpacity = 'text-opacity-50';
+
 function renderPage(currentPage) {
   if (currentPage === 'About') {
+    aboutOpacity = 'text-opacity-100';
+    portfolioOpacity = 'text-opacity-50';
+    contactOpacity = 'text-opacity-50';
+    resumeOpacity = 'text-opacity-50';
     return < About />
   }
   if (currentPage === 'Portfolio') {
+    aboutOpacity = 'text-opacity-50';
+    portfolioOpacity = 'text-opacity-100';
+    contactOpacity = 'text-opacity-50';
+    resumeOpacity = 'text-opacity-50';
     return < Portfolio />
   }
   if (currentPage === 'Contact') {
+    aboutOpacity = 'text-opacity-50';
+    portfolioOpacity = 'text-opacity-50';
+    contactOpacity = 'text-opacity-100';
+    resumeOpacity = 'text-opacity-50';
     return < Contact />
   }
   if (currentPage === 'Resume') {
+    aboutOpacity = 'text-opacity-50';
+    portfolioOpacity = 'text-opacity-50';
+    contactOpacity = 'text-opacity-50';
+    resumeOpacity = 'text-opacity-100';
     return < Resume />
   }
 };
 
 function App() {
   const [currentPage, setCurrentPage] = useState('About');
+
   return (
     <div>
       <div className="container-fluid bg-dark">
-      <div className="d-flex justify-content-center">
-        <h1 className="text-info m-2">Anthony's Portfolio</h1>
-      </div>
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <div className="d-flex justify-content-center collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav ">
-              <a onClick={() => setCurrentPage('About')} className="nav-link text-warning text-opacity-75">About</a>
-              <a onClick={() => setCurrentPage('Contact')} className="nav-link text-warning text-opacity-75">Contact</a>
-              <a onClick={() => setCurrentPage('Portfolio')} className="nav-link text-warning text-opacity-75">Portfolio</a>
-              <a onClick={() => setCurrentPage('Resume')} className="nav-link text-warning text-opacity-75">Resume</a>
+        <div className="d-flex justify-content-center">
+          <h1 className="text-info m-2">Anthony's Portfolio</h1>
+        </div>
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <div className="d-flex justify-content-center collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav ">
+                <a onClick={() => setCurrentPage('About')} className={`nav-link text-warning ${aboutOpacity}`}>About</a>
+                <a onClick={() => setCurrentPage('Contact')} className={`nav-link text-warning ${contactOpacity}`}>Contact</a>
+                <a onClick={() => setCurrentPage('Portfolio')} className={`nav-link text-warning ${portfolioOpacity}`}>Portfolio</a>
+                <a onClick={() => setCurrentPage('Resume')} className={`nav-link text-warning ${resumeOpacity}`}>Resume</a>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
       {renderPage(currentPage)}
     </div>
-
   )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='container'>
+      <h2 className='py-3 text-light'>Contact Me</h2>
+      <form>
+        <div className="mb-3">
+          <label className="form-label text-light">Name</label>
+          <input type="name" className="form-control" id="exampleFormControlInput1" placeholder="Your Name" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label text-light">Email Address</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label text-light">Message</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Enter Your Message Here" rows="3"></textarea>
+        </div>
+      </form>
+    </div>
   )
 }
 
